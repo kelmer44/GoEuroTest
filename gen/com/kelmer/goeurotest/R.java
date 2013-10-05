551475e6ea83107d17c9dbb8ab7a3d24c0bd9176
kelmer <kelmer@gmail.com>
2013-10-05 16:09:57 +0200

    Autocomplete

--- gen/com/kelmer/goeurotest/R.java
+++ gen/com/kelmer/goeurotest/R.java
@@ -10,6 +10,10 @@ package com.kelmer.goeurotest;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int holo_blue=0x7f040000;
+        public static final int holo_red=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +21,39 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_fav_location=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090008;
+        public static final int button1=0x7f090007;
+        public static final int dateLayout=0x7f090005;
+        public static final int dateView=0x7f090006;
+        public static final int from=0x7f090002;
+        public static final int fromView=0x7f090001;
+        public static final int linearLayout1=0x7f090000;
+        public static final int to=0x7f090004;
+        public static final int toView=0x7f090003;
     }
     public static final class layout {
         public static final int activity_search_form=0x7f030000;
+        public static final int list_item=0x7f030001;
     }
     public static final class menu {
-        public static final int search_form=0x7f070000;
+        public static final int search_form=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int date=0x7f060005;
+        public static final int error_no_internet=0x7f060006;
+        public static final int from=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int to=0x7f060004;
     }
     public static final class style {
         /** 
@@ -59,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/kelmer/goeurotest/SearchFormActivity.java
+++ src/com/kelmer/goeurotest/SearchFormActivity.java
@@ -1,23 +1,104 @@
 
 package com.kelmer.goeurotest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AutoCompleteTextView;
+
+import com.kelmer.goeurotest.model.Location;
 
 public class SearchFormActivity extends Activity {
-
+    
+    private boolean mChange = false;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_form);
+        
+        final AutoCompleteTextView from = (AutoCompleteTextView) findViewById(R.id.from);
+        from.setAdapter(new LocationAutoCompleteAdapter(this, R.layout.list_item));
+        from.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long rowId) {
+                setFrom((Location) parent.getItemAtPosition(position));
+                from.requestFocus();
+            }
+        });
+        from.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // clear location
+                setFrom(null);
+            }
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        });
+        
+     // To text input
+
+        final AutoCompleteTextView to = (AutoCompleteTextView) findViewById(R.id.to);
+        to.setAdapter(new LocationAutoCompleteAdapter(this, R.layout.list_item));
+        to.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long rowId) {
+                setTo((Location) parent.getItemAtPosition(position));
+                to.requestFocus();
+            }
+        });
+        to.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // clear location
+                setTo(null);
+            }
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.search_form, menu);
-        return true;
+    
+    private void setFrom(Location loc) {
+        if(!mChange) {
+            mChange = true;
+            AutoCompleteTextView fromView = (AutoCompleteTextView) findViewById(R.id.from);
+            fromView.setTag(loc);
+
+            if(loc != null) {
+                fromView.setText(loc.getName());
+                fromView.getBackground().setColorFilter(getResources().getColor(R.color.holo_blue), PorterDuff.Mode.SRC_ATOP);
+                fromView.dismissDropDown();
+            }
+            else {
+                fromView.getBackground().setColorFilter(getResources().getColor(R.color.holo_red), PorterDuff.Mode.SRC_ATOP);
+            }
+            mChange = false;
+        }
+    }
+    
+    private void setTo(Location loc) {
+        if(!mChange) {
+            mChange = true;
+            AutoCompleteTextView toView = (AutoCompleteTextView) findViewById(R.id.to);
+            toView.setTag(loc);
+
+            if(loc != null) {
+                toView.setText(loc.getName());
+                toView.getBackground().setColorFilter(getResources().getColor(R.color.holo_blue), PorterDuff.Mode.SRC_ATOP);
+                toView.dismissDropDown();
+            }
+            else {
+                toView.getBackground().setColorFilter(getResources().getColor(R.color.holo_red), PorterDuff.Mode.SRC_ATOP);
+            }
+            mChange = false;
+        }
     }
 
 }
